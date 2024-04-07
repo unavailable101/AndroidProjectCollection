697e6c9e4d46d5b737586bc233bd897de6db3532
unavailable101 <marga18nins@gmail.com>
2024-04-07 22:23:51 +0800

    colorMatch gana

--- app/src/main/java/com/example/androidprojectcollection/ColorMatching.java
+++ app/src/main/java/com/example/androidprojectcollection/ColorMatching.java
@@ -1,9 +1,11 @@
 package com.example.androidprojectcollection;
 
-import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,24 +16,19 @@ public class ColorMatching extends AppCompatActivity implements View.OnClickList
 
     Button exit, ret;
     Button[][] btn = new Button[3][3];
-
-    int[] btn_id = {
-        R.id.btn1,
-        R.id.btn2,
-        R.id.btn3,
-        R.id.btn4,
-        R.id.btn5,
-        R.id.btn6,
-        R.id.btn7,
-        R.id.btn8,
-        R.id.btn9
+    TextView secret_cheat;
+    int[][] btn_id = {
+            { R.id.btn1, R.id.btn2, R.id.btn3 },
+            { R.id.btn4, R.id.btn5, R.id.btn6 },
+            { R.id.btn7, R.id.btn8, R.id.btn9 }
     };
-
     int[] colors = {
-            Color.CYAN,
-            Color.BLACK,
-            Color.BLUE
+            R.color.rand1,
+            R.color.rand2,
+            R.color.rand3
     };
+    int[][] blocks = new int[3][3];
+    Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,16 +37,15 @@ public class ColorMatching extends AppCompatActivity implements View.OnClickList
 
         ret = (Button) findViewById(R.id.ret);
         exit = (Button) findViewById(R.id.exit);
+        secret_cheat = (TextView) findViewById(R.id.secret_cheat);
 
-
-        Random random = new Random();
-        int k = 0;
         for (int i = 0; i<3; i++){
             for (int j = 0; j<3; j++){
                 int random_color = random.nextInt(colors.length);
-                btn[i][j] = (Button) findViewById(btn_id[k++]);
+                btn[i][j] = (Button) findViewById(btn_id[i][j]);
+                btn[i][j].setBackgroundColor( getColor(colors[random_color]) );
+                blocks[i][j] = random_color;
                 btn[i][j].setOnClickListener(this);
-                btn[i][j].setBackgroundColor(colors[random_color]);
             }
         }
         exit.setOnClickListener(new View.OnClickListener() {
@@ -62,59 +58,79 @@ public class ColorMatching extends AppCompatActivity implements View.OnClickList
         ret.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                for(int i = 0; i<3; i++){
-                    for (int j = 0; j<3; j++){
-                        int random_color = random.nextInt(colors.length);
-                        btn[i][j].setBackgroundColor(colors[random_color]);
-                    }
-                }
+                playAgain();
             }
         });
+
+        secret_cheat.setOnClickListener(this);
     }
     @Override
     public void onClick(View v){
-//        Toast.makeText(getApplicationContext(), "it clicked!" + v.getId(), Toast.LENGTH_SHORT).show();
-        int curr = v.getId();
-        int curr_in = 0;
-        for (int i = 0; i<btn_id.length; i++){
-            if (curr == btn_id[i]) curr_in = i;
-        }
-        if (!check()) {
-            changeColor(curr_in);
-        } else {
-            Toast.makeText(getApplicationContext(), "YOU WIN!", Toast.LENGTH_SHORT).show();
-        }
-    }
 
-    public void changeColor(int ind){
-        for (int i = 0; i<3; i++){//row
-            for(int j = 0; j<3; j++){//col
-                if (btn[i][j].getId() == btn_id[ind]){
-                    if (i > 0){
-                        btn[i-1][j].setBackgroundColor(colors[(btn[i-1][j].getSolidColor()+1)%3]);
-                    }
-                    if (i < 0){
-                        btn[i+1][j].setBackgroundColor(colors[(btn[i+1][j].getSolidColor()+1)%3]);
-                    }
-                    if (j > 0){
-                        btn[i][j-1].setBackgroundColor(colors[(btn[i][j-1].getSolidColor()+1)%3]);
-                    }
-                    if ( j < 0 ){
-                        btn[i][j+1].setBackgroundColor(colors[(btn[i][j+1].getSolidColor()+1)%3]);
-                    }
+        if (v.getId() == R.id.secret_cheat){
+            int random_color = random.nextInt(colors.length);
+            for (int i = 0; i<3; i++){
+                for (int j = 0; j<3; j++){
+                    btn[i][j].setBackgroundColor( getColor(colors[random_color]) );
+                    blocks[i][j] = random_color;
                 }
             }
         }
+        //i = row
+        //j = col
+        if ( !colorMatch() ){
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (btn[i][j].getId() == v.getId()) {
+                        if (i > 0)
+                            btn[i - 1][j].setBackgroundColor( getColor(colors[blocks[i - 1][j] = nextColor(blocks[i - 1][j])]) );
+                        if (i < 2)
+                            btn[i + 1][j].setBackgroundColor( getColor(colors[blocks[i + 1][j] = nextColor(blocks[i + 1][j])]) );
+                        if (j > 0)
+                            btn[i][j - 1].setBackgroundColor( getColor(colors[blocks[i][j - 1] = nextColor(blocks[i][j - 1])]) );
+                        if (j < 2)
+                            btn[i][j + 1].setBackgroundColor( getColor(colors[blocks[i][j + 1] = nextColor(blocks[i][j + 1])]) );
+                        break;
+                    }
+                }
+            }
+        } else {
+            new CountDownTimer(1000, 1000){
+                @Override
+                public void onTick(long l) {
+                    Toast.makeText(getApplicationContext(), "You Win! All blocks are matched!", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFinish() {
+                    playAgain();
+                }
+            }.start();
+        }
     }
 
-    public boolean check(){
-        Button intitial = btn[0][0];
-        for (int i=0; i<3; i++){
+    private int nextColor(int curr){
+        return curr+1 < colors.length ? (curr+1) : 0;
+    }
+
+    private boolean colorMatch(){
+        int initial = blocks[0][0];
+        int count = 1;
+        for (int i = 0; i<3; i++){
             for (int j = 0; j<3; j++){
-                if (btn[i][j] != intitial) return false;
+                if (initial == blocks[i][j]) count++;
             }
         }
+        return count == 10;
+    }
 
-        return true;
+    public void playAgain(){
+        for(int i = 0; i<3; i++){
+            for (int j = 0; j<3; j++){
+                int random_color = random.nextInt(colors.length);
+                btn[i][j].setBackgroundColor( getColor(colors[random_color]) );
+                blocks[i][j] = random_color;
+            }
+        }
     }
 }
